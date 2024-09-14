cbc1e3a845b41459a7d0d4e8bcba37e7f4978403
Raul559a <rayoncal20@gmail.com>
2024-09-13 18:16:48 -0600

    Update ParImpar.py

--- Condicionales/Retos/ParImpar.py
+++ Condicionales/Retos/ParImpar.py
@@ -1,1 +1,9 @@
 ```Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.```
+
+number = int(input('Choose your nunmber:'))
+
+if number % 2 == 0: 
+    print("It's pair")
+
+else:
+    print('the number is odd')
